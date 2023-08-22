fdcb2b00e4443b4d16c2523bc4a303fd160f16d9
Devshe <sheroniperera20@gmail.com>
2023-08-22 12:44:28 +0530

    product frontend with CRD functions

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,7 +1,13 @@
 export { default as NotFound } from './404';
 export { default as Home } from './home';
 export { default as Users } from './users';
+export { default as Login } from './auth/login';
+export { default as Register } from './auth/register';
+export { default as Verify } from './auth/verify';
+export { default as Products } from './product/product.jsx';
+export { default as ProductForm } from './product/productAdd.jsx';
 export { default as Contact } from './contact';
 export { default as Profile } from './profile';
-
+export { default as ProductDetail } from './product/productDetail.jsx';
+export { default as Order } from './order/order.jsx';
 export * from './auth';
